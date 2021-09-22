2186967eba09fd9d9cbe024bb5c4d25d8352e61d
Bijayani Mishra <38001077+bijayaniSMishra@users.noreply.github.com>
2021-09-22 15:39:38 -0700

    Create __init__.py

---
+++ application/__init__.py
@@ -0,0 +1,39 @@
+# -*- encoding: utf-8 -*-
+from flask import Flask, url_for
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from importlib import import_module
+from logging import basicConfig, DEBUG, getLogger, StreamHandler
+from os import path
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def register_extensions(app):
+    db.init_app(app)
+    login_manager.init_app(app)
+
+def register_blueprints(app):
+    for module_name in ('base', 'home'):
+        module = import_module('application.{}.routes'.format(module_name))
+        app.register_blueprint(module.blueprint)
+
+def configure_database(app):
+
+    @app.before_first_request
+    def initialize_database():
+        db.create_all()
+
+    @app.teardown_request
+    def shutdown_session(exception=None):
+        db.session.remove()
+
+def create_app(config):
+    application = Flask(__name__, static_folder='base/static')
+    app = application
+    application.config.from_object(config)
+    register_extensions(application)
+    register_blueprints(application)
+    configure_database(application)
+    return application
+
